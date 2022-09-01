ad51ddb98877b09974f5a6a383454d592c3a5e56
Naveen <Naveen.b@technovert.com>
2022-09-01 18:09:20 +0530

    Completed

--- index.js
+++ index.js
@@ -17,7 +17,40 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
+const getResponse = (date) => {
+  const days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  let dateString = `${days[date.getUTCDay()]}, ${String(date.getUTCDate()).padStart(2,'0')} ${months[date.getUTCMonth()]} ${date.getUTCFullYear()} ${String(date.getUTCHours()).padStart(2,'0')}:${String(date.getUTCMinutes()).padStart(2,'0')}:${String(date.getUTCSeconds()).padStart(2,'0')} GMT`
+  console.log(date);
+  let unix = date/1;
+  return ({
+    "unix":unix,
+    "utc":dateString
+  })
+}
+app.get("/api/:date",(req,res)=>{
+  let date=null;
+  try {
+    date = new Date(req.params.date);
+  } catch(err) {
 
+  }
+  if(date===null || date == "Invalid Date") {
+    try{
+      date = new Date(parseInt(req.params.date))
+    } catch(err) {
+      res.json({ error : "Invalid Date" })
+    }
+  }
+  if(date == "Invalid Date") {
+    res.json({ error : "Invalid Date" }) 
+  }
+  res.json(getResponse(date))
+})
+app.get("/api",(req,res)=> {
+  let date = new Date();
+  res.json(getResponse(date))
+})
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
@@ -27,6 +60,6 @@ app.get("/api/hello", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
